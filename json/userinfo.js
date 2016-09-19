6fb66cce42f1932256bbd85a84e698060602d6a6
abalita <abalita@LotC-Ext-PC1>
2016-09-19 16:48:14 +0800

    Initial Commit

---
+++ json/userinfo.js
@@ -0,0 +1,11 @@
+var accounts =[
+{
+"email": "mark@gmail.com",
+"password": "mark123",
+"firstname": "Mark Jhon Paul",
+"middlename": "Garcia",
+"lastname": "Quijom",
+"suffix": "Jr.",
+"birthdate": "1995-05-25"
+}
+];
